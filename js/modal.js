d28f9ec21ae5f2eea45709b2c7debfb23d353d8f
natalymois <mois-2011@mail.ru>
2017-04-05 18:50:51 +0300

    html, css, Javascript
    Разработка модальных окон, подключение их с помощью самописного jQuery

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+$(function(){
+
+   /*-----При клике на блок входа всплывает модальное окно личного кабинета----*/
+   var flag = 0;
+   $('.user-block').click(function(event){
+	  
+	  $('.modal-content').slideToggle(1000);
+   });
+  
+   $('.modal-content-close').click(function(e){
+	  $('.modal-content').css('display','none');
+   });
+   
+   /*---- Второе модальное окно: карта ----*/
+   /*Клик на ссылку в футере*/
+   $('.footer-contacts a').click(function(event){
+	  
+	  $('.modal-content-map').slideToggle(1000);
+   });
+   
+   $('.modal-content-close').click(function(e){
+	  $('.modal-content-map').css('display','none');
+   });
+   
+   /*Клик в блоке "Контактная информация: как проехать"*/
+   
+   $('.main-content-left .btn-map').click(function(event){
+   
+	  event.preventDefault();
+	  
+	  $('.modal-content-map').slideToggle(1000);
+   });
+   
+   $('.modal-content-close').click(function(e){
+	  $('.modal-content-map').css('display','none');
+   });
+});
+   
